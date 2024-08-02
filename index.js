c1c74364afa60b434ae3eb015a4979acb94ffeb6
MrBarker47 <jebarker7@gmail.com>
2024-08-02 13:12:47 -0400

    Update files

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 // Create 256 grid
 for (let i = 0; i < 256; i++) {
-  newDiv = document.createElement("div");
+  let newDiv = document.createElement("div");
   newDiv.id = "color";
-  newDiv.classList.add("hover");
+  newDiv.addEventListener("mouseover", (event) => {
+    newDiv.style.backgroundColor = "red";
+  });
   container.appendChild(newDiv);
-}
-
-//Hover
-document.getElementById("color").addEventListener("mouseover", mouseOut);
-
-function mouseOut() {
-  document.getElementById("color").style.backgroundColor = "white";
 }
 
 //Create a button at the top of the screen
