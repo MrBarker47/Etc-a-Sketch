ab485335338727ba1d077be7d2e08858d3006490
MrBarker47 <jebarker7@gmail.com>
2024-08-08 15:46:29 -0400

    Updated files

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 // Create 256 grid
-for (let i = 0; i < 256; i++) {
-  let newDiv = document.createElement("div");
-  newDiv.id = "color";
-  newDiv.addEventListener("mouseover", (event) => {
-    newDiv.style.backgroundColor = "red";
-  });
-  container.appendChild(newDiv);
+
+// for (let i = 0; i < 256; i++) {
+//   let newDiv = document.createElement("div");
+//   newDiv.id = "color";
+//   newDiv.addEventListener("mouseover", (event) => {
+//     newDiv.style.backgroundColor = "black";
+//   });
+//   container.appendChild(newDiv);
+// }
+
+function fullSize() {
+  for (let i = 0; i < 256; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.id = "color";
+    newDiv.addEventListener("mouseover", (event) => {
+      newDiv.style.backgroundColor = "black";
+    });
+    container.appendChild(newDiv);
+  }
+}
+
+function grid() {
+  let newGrid = prompt("Change size?");
+  if ((newGrid = 64)) {
+  }
+  for (let i = 0; i < 4096; i++) {
+    let newSize = document.createElement("div");
+    newSize.classList.add("content");
+    newSize.addEventListener("mouseover", (event) => {
+      newSize.style.backgroundColor = "black";
+    });
+    container.appendChild(newSize);
+  }
 }
 
 //Create a button at the top of the screen
@@ -16,8 +42,9 @@ button.appendChild(topOfScreen);
 //button to change the size of the grid
 function newButton() {
   topOfScreen.addEventListener("click", (e) => {
-    console.log("Hello");
+    grid();
   });
 }
 
+fullSize();
 newButton();
